a2276619f155b9a819dc2aabd5cb0562003d613a
Thomas Hansen <Tom.Hansen.II@gmail.com>
2025-08-20 14:38:00 +0000

    Gemini - take it easy homie.  Lighten up.  You're doing great, be positi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,28 +3,23 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "paper-street-soapbox",
-  "appId": "1:762380655759:web:a9c1855b0308b713b824fa",
-  "storageBucket": "paper-street-soapbox.firebasestorage.app",
-  "apiKey": "AIzaSyD1FGmC2Xquifxfzrm-a-lg7MsOZrhOtDk",
-  "authDomain": "paper-street-soapbox.firebaseapp.com",
-  "messagingSenderId": "762380655759"
+  apiKey: "AIzaSyD1FGmC2Xquifxfzrm-a-lg7MsOZrhOtDk",
+  authDomain: "paper-street-soapbox.firebaseapp.com",
+  projectId: "paper-street-soapbox",
+  storageBucket: "paper-street-soapbox.firebasestorage.app",
+  messagingSenderId: "762380655759",
+  appId: "1:762380655759:web:a9c1855b0308b713b824fa",
 };
 
 
 // Initialize Firebase
 let app: FirebaseApp;
-let auth: Auth;
-
 if (!getApps().length) {
-  if (!firebaseConfig.apiKey) {
-    throw new Error('Missing Firebase API Key');
-  }
   app = initializeApp(firebaseConfig);
 } else {
   app = getApp();
 }
 
-auth = getAuth(app);
+const auth = getAuth(app);
 
 export { app, auth };
