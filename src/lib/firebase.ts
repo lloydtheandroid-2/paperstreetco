2ec97fbf66055ddc5259a60753269f737bcdee9f
Thomas Hansen <Tom.Hansen.II@gmail.com>
2025-08-20 15:28:25 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1FGmC2Xquifxfzrm-a-lg7MsOZrhOtDk",
-  authDomain: "paper-street-soapbox.web.app",
-  projectId: "paper-street-soapbox-84i9i",
-  storageBucket: "paper-street-soapbox-84i9i.appspot.com",
-  messagingSenderId: "762380655759",
-  appId: "1:762380655759:web:a9c1855b0308b713b824fa",
+  "projectId": "paper-street-soapbox-84i9i",
+  "appId": "1:464035810053:web:1c35cc31f0c653a73e456c",
+  "storageBucket": "paper-street-soapbox-84i9i.appspot.com",
+  "apiKey": "AIzaSyAE8ntCP5G7_Y0WH7ik0Db05U8CT3JJ2rs",
+  "authDomain": "paper-street-soapbox-84i9i.firebaseapp.com",
+  "messagingSenderId": "464035810053"
 };
 
 function getFirebaseApp(): FirebaseApp {
